3a9ae32beae49dd33566c58809e505f7ea0ce306
Annie <anniebeltran23@gmail.com>
2018-12-29 11:09:20 -0800

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name) values ('Jr. Bacon Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Happy Meal Burger');
